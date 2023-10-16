f5dd5e2df27d51aebf5b2b8b43a55cd5531e757c
LeeTaeGwan <>
2023-10-17 01:20:46 +0900

    feat: header title 타입 정의

---
+++ src/constants/url.ts
@@ -0,0 +1,7 @@
+export const INDEX = '/';
+export const CLIPS = '/user/clip';
+export const INGEST = '/ingest';
+export const ADMIN_MANAGEMENT_TEAM = '/admin/management/team';
+export const ADMIN_MANAGEMENT_USER = '/admin/management/user';
+export const ADMIN_MANAGEMENT_DEPT = '/admin/management/dept';
+export const ADMIN_MANAGEMENT_RANK = '/admin/management/rank';
---
+++ src/utils/setHeaderTitle.ts
@@ -0,0 +1,27 @@
+import {
+  CLIPS,
+  INGEST,
+  ADMIN_MANAGEMENT_TEAM,
+  ADMIN_MANAGEMENT_USER,
+  ADMIN_MANAGEMENT_DEPT,
+  ADMIN_MANAGEMENT_RANK,
+} from '@src/constants/url';
+
+export const setHeaderTitle = (pathname: string) => {
+  switch (pathname) {
+    case CLIPS:
+      return '클립';
+    case INGEST:
+      return '인제스트';
+    case ADMIN_MANAGEMENT_TEAM:
+      return '팀 관리';
+    case ADMIN_MANAGEMENT_USER:
+      return '유저 관리';
+    case ADMIN_MANAGEMENT_DEPT:
+      return '부서 관리';
+    case ADMIN_MANAGEMENT_RANK:
+      return '직급 및 계급 관리';
+    default:
+      return '뭔가 에러가 났음';
+  }
+};
