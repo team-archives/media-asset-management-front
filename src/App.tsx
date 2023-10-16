30983384b670868e60e0fa73ad18ddce273c98ff
LeeTaeGwan <>
2023-10-17 04:11:09 +0900

    chore: App Route컴포넌트 path string이 아니라 변수로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import IndexPage from '@src/Page/Index';
 import { Route, Routes } from 'react-router-dom';
 import Layout from '@src/layout/Layout';
+import {
+  INDEX_URL,
+  CLIPS_URL,
+  INGEST_URL,
+  ADMIN_MANAGEMENT_TEAM_URL,
+  ADMIN_MANAGEMENT_USER_URL,
+  ADMIN_MANAGEMENT_DEPT_URL,
+  ADMIN_MANAGEMENT_RANK_URL,
+} from '@src/constants/url';
+import IndexPage from '@src/Page/Index';
 import ClipManagement from '@src/Page/Clips/ClipManagement';
+import Ingest from '@src/Page/Clips/Ingest';
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/user/clip" element={<ClipManagement />} />
-        <Route path="/user/clip" element={<ClipManagement />} />
-        {/* Using path="*"" means "match anything", so this route
-            acts like a catch-all for URLs that we don't have explicit
-            routes for. */}
-        {/* <Route path="*" element={<NoMatch />} /> */}
+        <Route path={INDEX_URL} element={<IndexPage />} />
+        <Route path={CLIPS_URL} element={<ClipManagement />} />
+        <Route path={INGEST_URL} element={<Ingest />} />
+        <Route path={ADMIN_MANAGEMENT_TEAM_URL} element={<IndexPage />} />
+        <Route path={ADMIN_MANAGEMENT_USER_URL} element={<IndexPage />} />
+        <Route path={ADMIN_MANAGEMENT_DEPT_URL} element={<IndexPage />} />
+        <Route path={ADMIN_MANAGEMENT_RANK_URL} element={<IndexPage />} />
       </Routes>
     </Layout>
   );
--- src/utils/setLayoutTitles.ts
+++ src/utils/setLayoutTitles.ts
@@ -1,25 +1,25 @@
 import {
-  CLIPS,
-  INGEST,
-  ADMIN_MANAGEMENT_TEAM,
-  ADMIN_MANAGEMENT_USER,
-  ADMIN_MANAGEMENT_DEPT,
-  ADMIN_MANAGEMENT_RANK,
+  CLIPS_URL,
+  INGEST_URL,
+  ADMIN_MANAGEMENT_TEAM_URL,
+  ADMIN_MANAGEMENT_USER_URL,
+  ADMIN_MANAGEMENT_DEPT_URL,
+  ADMIN_MANAGEMENT_RANK_URL,
 } from '@src/constants/url';
 
 export const setHeaderTitle = (pathname: string) => {
   switch (pathname) {
-    case CLIPS:
+    case CLIPS_URL:
       return '클립';
-    case INGEST:
+    case INGEST_URL:
       return '인제스트';
-    case ADMIN_MANAGEMENT_TEAM:
+    case ADMIN_MANAGEMENT_TEAM_URL:
       return '팀 관리';
-    case ADMIN_MANAGEMENT_USER:
+    case ADMIN_MANAGEMENT_USER_URL:
       return '유저 관리';
-    case ADMIN_MANAGEMENT_DEPT:
+    case ADMIN_MANAGEMENT_DEPT_URL:
       return '부서 관리';
-    case ADMIN_MANAGEMENT_RANK:
+    case ADMIN_MANAGEMENT_RANK_URL:
       return '직급 및 계급 관리';
     default:
       return '뭔가 에러가 남';
@@ -28,17 +28,17 @@ export const setHeaderTitle = (pathname: string) => {
 
 export const setSidebarTitle = (pathname: string) => {
   switch (pathname) {
-    case CLIPS:
+    case CLIPS_URL:
       return '클립관리';
-    case INGEST:
+    case INGEST_URL:
       return '인제스트';
-    case ADMIN_MANAGEMENT_TEAM:
+    case ADMIN_MANAGEMENT_TEAM_URL:
       return '팀 & 프로그램 관리';
-    case ADMIN_MANAGEMENT_USER:
+    case ADMIN_MANAGEMENT_USER_URL:
       return '유저 관리';
-    case ADMIN_MANAGEMENT_DEPT:
+    case ADMIN_MANAGEMENT_DEPT_URL:
       return '부서 관리';
-    case ADMIN_MANAGEMENT_RANK:
+    case ADMIN_MANAGEMENT_RANK_URL:
       return '계급 관리';
     default:
       return '뭔가 에러가 남';
