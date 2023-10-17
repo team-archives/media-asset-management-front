2d6a81871ec7a06ca3042b97a6943187418b27d2
LeeTaeGwan <>
2023-10-17 22:31:06 +0900

    style: 기본 Footer 디자인

--- src/components/CommonStyledComponents/CommonStyledComponents.ts
+++ src/components/CommonStyledComponents/CommonStyledComponents.ts
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
 
 export const ContentContainer = styled.div`
+  height: 100%;
   display: flex;
   gap: 1rem;
   padding: 1rem;
+  background-color: ${({ theme }) => theme.palette.cnps.backgroundColor};
 `;
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,9 +1,39 @@
 import styled from '@emotion/styled';
 
+const DUMMY_DATA = [1, 2, 3, 4, 5];
+
 const Footer = () => {
-  return <FooterContainer>Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      {DUMMY_DATA.map((data) => (
+        <SquareBox key={data}>{data}</SquareBox>
+      ))}
+    </FooterContainer>
+  );
 };
 
-const FooterContainer = styled.div``;
+const FooterContainer = styled.div`
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.palette.cnps.realWhite};
+  color: ${({ theme }) => theme.palette.cnps.black};
+  border-top: solid 0.01rem #777777;
+`;
+
+const SquareBox = styled.div`
+  width: 2rem;
+  height: 2rem;
+  font-size: larger;
+  color: ${({ theme }) => theme.palette.cnps.black};
+  transition: background-color 0.3s ease-in-out;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  :hover {
+    background-color: #777777;
+  }
+`;
 
 export default Footer;
