f1d24d39d6b386e463f4091e545df2f836c081c8
LeeTaeGwan <>
2023-10-19 00:31:40 +0900

    feat: API요청시 쿠키를 헤더에 넣어서 보내도록 수정

--- src/api/Base/ApiClient.ts
+++ src/api/Base/ApiClient.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosResponse } from 'axios';
+import { getCookie } from '@src/utils/Cookie';
 
 export default class ApiClient {
   // Todo: jwt token logic to be implemented
-  // static accessToken = getCookie('accessToken');
+  static accessToken = getCookie('accessToken');
   static instance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    // withCredentials: true,
-    // headers: this.accessToken ? { Authorization: `Bearer ${this.accessToken}` ?? '' } : {},
+    withCredentials: true,
+    headers: this.accessToken ? { Authorization: `Bearer ${this.accessToken}` ?? '' } : {},
   });
 
   static get<T = any, R = AxiosResponse<T>, D = any>(
